7e3f6bdcefa6ea4e7e0daf3dacaddad82aee66d1
prabhu34 <18209477+prabhu34@users.noreply.github.com>
2022-01-24 10:54:17 +0530

    chore: updated versions

--- versions.tf
+++ versions.tf
@@ -20,11 +20,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.7"
+      version = "~> 4.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.7"
+      version = "~> 4.0"
     }
   }
 
